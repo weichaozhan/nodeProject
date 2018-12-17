6629f192c3eeefe2b3b817b24a1c6d4e91768f55
詹伟超 <weichao.zhan@kangcenet.com>
2018-12-17 15:43:11 +0800

    mkdir

--- back/app.js
+++ back/app.js
@@ -1,18 +1,3 @@
-// import express from 'express';
-// import path from 'path';
-
-// const app = express();
-
-// app.get('/helloworld', (req, res) => {
-//     res.send('Hello World');
-// });
-
-// app.use(express.static('public'));
-
-// const server = app.listen(3000, () => {
-//     console.log(`Server listening on port %d`, server.address().port);
-// });
-
 import express from 'express';
 import configLite from 'config-lite';
 import router from './routes/index.js';
--- back/config/default.js
+++ back/config/default.js
@@ -1,15 +1,15 @@
 'use strict';
-// console.log(process.env)
+
 module.exports = {
-	port: parseInt(process.env.PORT, 10) || 8001,
+	port: parseInt(process.env.PORT, 10) || 3000,
 	url: 'mongodb://localhost:27017/elm',
 	session: {
 		name: 'SID',
 		secret: 'SID',
 		cookie: {
 			httpOnly: true,
-	    secure:   false,
-	    maxAge:   365 * 24 * 60 * 60 * 1000,
+            secure:   false,
+            maxAge:   365 * 24 * 60 * 60 * 1000,
 		}
 	}
 }
